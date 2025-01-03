7890b69f0b05369188ad5c928549b4583466858c
Misha <mishazhizhka@gmail.com>
2025-01-03 18:34:37 +0200

    fixing js and css1

--- Newsify/settings.py
+++ Newsify/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 
 import os
 
+import mimetypes
+
+mimetypes.add_type("text/javascript", ".js", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
